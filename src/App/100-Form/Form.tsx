4f276af1ed2275db9fd0f367cb1a47f8abf73e8c
fable54321 <timothe5@hotmail.com>
2026-06-04 12:55:12 -0400

    first commit

--- src/App/100-Form/Form.tsx
+++ src/App/100-Form/Form.tsx
@@ -2,12 +2,18 @@
 
 const Form = () => {
   return (
-    <section>
-      <form action="">
-        <label htmlFor="name">
+    <section className="w-full flex flex-col items-center">
+      <form action="" className="flex flex-col gap-4 mt-10  border border-secondary p-4">
+        <label htmlFor="name" className="flex gap-2">
           Nom:
+        
+            <input className="basic-input" type="text" id="name" name="name" />
         </label>
-        <input className="basic-input" type="text" id="name" name="name" />
+        <label className="w-full flex flex-col">
+            Description du produit à acheter: 
+            <textarea className="basic-input mx-auto" name="description" id="description" cols={30} rows={5}></textarea>
+        </label>
+        
       </form>
     </section>
   )
