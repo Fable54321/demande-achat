b7d93c95d2861b60344a9910440e93cbbfa70a16
fable54321 <timothe5@hotmail.com>
2026-06-04 13:21:59 -0400

    first commit

--- src/App/100-Form/Form.tsx
+++ src/App/100-Form/Form.tsx
@@ -3,29 +3,34 @@
 const Form = () => {
   return (
     <section className="w-full flex flex-col items-center">
-      <form action="" className="flex flex-col gap-4 mt-6  border border-secondary p-4 rounded-lg">
+      <form action="" className="bg-[#e8efe2] flex flex-col w-[min(90%,30rem)] gap-4 mt-6  border border-secondary p-4 rounded-lg">
         <label htmlFor="name" className="flex flex-col gap-1">
           Nom:
         
-            <input className="basic-input" type="text" id="name" name="name" />
+            <input className="basic-input bg-white" type="text" id="name" name="name" />
         </label>
         <label className="w-full flex flex-col gap-1">
             Description du produit à acheter: 
-            <textarea className="basic-input mx-auto" name="description" id="description" cols={30} rows={3}></textarea>
+            <textarea className="basic-input mx-auto w-full bg-white" name="description" id="description" cols={30} rows={3}></textarea>
         </label>
         <label  className="w-full flex flex-col gap-1">
         Justification de l'achat:
-        <textarea className="basic-input mx-auto" name="justification" id="justification" cols={30} rows={3}></textarea>
+        <textarea className="basic-input mx-auto w-full bg-white" name="justification" id="justification"  rows={3}></textarea>
         </label>
 
         <label className="w-full flex flex-col gap-1">
             Prix connu ou estimé:
-            <input className="basic-input" type="text" name="price" id="price" />
+            <input className="basic-input bg-white" type="text" name="price" id="price" />
         </label>
 
         <label className="w-full flex flex-col gap-1">
             Lien vers le produit (si possible):
-            <input className="basic-input" type="text" name="link" id="link" />
+            <input className="basic-input bg-white" type="text" name="link" id="link" />
+        </label>
+
+        <label>
+            Sélectionner la date à laquelle le produit est attendu:
+            <input className="basic-input bg-white" type="date" name="expectedDate" id="expectedDate" />
         </label>
         
       </form>
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,8 +4,10 @@ import { Outlet } from "react-router-dom"
 const App = () => {
   return (
     <article className="bg-tertiary font-tertiary flex flex-col w-full items-center">
-      <h1 className="font-primary font-bold text-secondary mt-6 text-[2.5em] ">Formulaire de demande d'achat</h1>
+      <div className="mt-8 shadow-2xl bg-white p-4 rounded-2xl">
+        <h1 className="font-tertiary font-bold text-secondary mt-4 text-[2.5em] ">Formulaire de demande d'achat</h1>
       <Outlet />
+      </div>
     </article>
   )
 }
